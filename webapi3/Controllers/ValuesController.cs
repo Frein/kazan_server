7cf60865b2181150088e83375348a67d71d45d89
sovchinnikov <gross17@yandex.ru>
2019-09-24 18:17:32 +0300

    first

---
+++ webapi3/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContosoUniversity.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public SchoolContext Context { get; }
+
+        public ValuesController(SchoolContext context):base() {
+            Context = context;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Student>> Get()
+        {
+            return this.Context.Student.ToList();
+        }
+
+     
+    }
+}
